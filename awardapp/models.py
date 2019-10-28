94b85e41b28ca6ab0b7b74afae7526237ff703e2
mfannick <mfannick1@gmail.com>
2019-10-28 02:07:16 +0200

    for database tables

---
+++ awardapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Project(models.Model):
+    userF=models.ForeignKey(User,on_delete=models.CASCADE)
+    projectName=models.CharField(max_length=30)
+    projectImage=models.ImageField(upload_to='projectImg/')
+    projectDescription=models.TextField()
+    projectUrl=models.URLField()
+    projectTime=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.projectName
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profileImage=models.ImageField(default='default.jpg',upload_to='profileImg/')
+    profileBio=models.TextField()
+    profileEmail=models.EmailField()
+
+    def __str__(self):
+        return self.user.username
